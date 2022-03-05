2d82f309b986c419ae3593e118ed45bb4eeae9fd
LaironLF <dimaklassk@gmail.com>
2022-03-05 14:43:04 +0700

    1 изменение

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp12
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("No Hello World! >:(");
+            
         }
     }
 }
